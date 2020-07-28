3fabe93a3a69a6684c4a826c1caf49c09f5abf4c
jacobdavis2 <jacob.davis@revature.com>
2020-07-28 20:20:33 +0000

    Created breakfast functions that can actually be isolated into Tasks.

--- Challenge/Program.cs
+++ Challenge/Program.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Diagnostics;
 
 /* Your Challenge: given the following functions:
      1. First, build and run the program to see how long it takes synchronously.
-     2. Next, modify the method headers and the places they are called such that
-         the methods run asynchronously.
+     2. Next, modify any of the given code you like such that you use async/await
+        to improve run time.
      3. Finally, build and run again, and observe the difference in speed and
          order of execution.
+        
+     As a hint, research for yourself when to use async/await and how to achieve
+     concurrent processing using said methodology.
 */
 
 namespace Challenge
@@ -17,27 +21,27 @@ namespace Challenge
         static public void ScrambleEggs()
         {
             Console.WriteLine("Starting the eggs...");
-            Thread.Sleep(10000);
+            for (long i = 0; i < 1000000000; ++i) Math.Sqrt(i);
             Console.WriteLine("Eggs are finished!");
         }
 
         static public void ToastToast()
         {
             Console.WriteLine("Toasting the toast...");
-            Thread.Sleep(5000);
+            for (long i = 0; i < 500000000; ++i) Math.Sqrt(i);
             Console.WriteLine("Toast is toasted!");
         }
 
         static public void PourOrangeJuice()
         {
             Console.WriteLine("Pouring some juice...");
-            Thread.Sleep(1000);
+            for (long i = 0; i < 100000000; ++i) Math.Sqrt(i);
             Console.WriteLine("Orange juice poured!");
         }
 
         // Some code was taken directly from the Microsoft docs here:
         //  https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.stopwatch?view=netcore-3.1
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
