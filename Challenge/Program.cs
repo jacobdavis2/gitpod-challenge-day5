ce0af6ac9f163db472bfb428bcc0dfa9cf66ced3
jacobdavis2 <jacob.davis@revature.com>
2020-07-28 19:48:50 +0000

    First draft complete.

--- Challenge/Program.cs
+++ Challenge/Program.cs
@@ -1,12 +1,56 @@
-﻿using System;
+using System;
+using System.Threading;
+using System.Diagnostics;
+
+/* Your Challenge: given the following functions:
+     1. First, build and run the program to see how long it takes synchronously.
+     2. Next, modify the method headers and the places they are called such that
+         the methods run asynchronously.
+     3. Finally, build and run again, and observe the difference in speed and
+         order of execution.
+*/
 
 namespace Challenge
 {
     class Program
     {
+        static public void ScrambleEggs()
+        {
+            Console.WriteLine("Starting the eggs...");
+            Thread.Sleep(10000);
+            Console.WriteLine("Eggs are finished!");
+        }
+
+        static public void ToastToast()
+        {
+            Console.WriteLine("Toasting the toast...");
+            Thread.Sleep(5000);
+            Console.WriteLine("Toast is toasted!");
+        }
+
+        static public void PourOrangeJuice()
+        {
+            Console.WriteLine("Pouring some juice...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Orange juice poured!");
+        }
+
+        // Some code was taken directly from the Microsoft docs here:
+        //  https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.stopwatch?view=netcore-3.1
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            ScrambleEggs();
+            ToastToast();
+            PourOrangeJuice();
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}", ts.Seconds, ts.Milliseconds / 10);
+            Console.WriteLine("Time it took to cook breakfast: " + elapsedTime + " seconds.");
         }
     }
 }
